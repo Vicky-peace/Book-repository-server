1e664828188a7f913926de7acdfe61f17c7ec95e
Vicky-peace <victorpeace47@gmail.com>
2024-06-26 15:58:46 +0300

    server and routes working

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -12,3 +12,14 @@ export const getAllBooks = async(c: Context) => {
     }
 }
 
+
+export const getBook = async(c: Context) => {
+    try {
+        const id = Number(c.req.param('id'));
+        const book = await getBookService(id);
+        return book ? c.json(book) : c.json({error: "Book not found"}, 404);
+    } catch (error: any) {
+        return c.json({error: error.message}, 500);
+    }
+   
+}
--- src/book/book.router.ts
+++ src/book/book.router.ts
@@ -1,7 +1,8 @@
 import { Hono } from "hono";
-import { getAllBooks } from "./book.controller";
+import { getAllBooks,getBook } from "./book.controller";
 
 
 export const booksRouter = new Hono();
 
 booksRouter.get('/books', getAllBooks);
+booksRouter.get('/books/:id', getBook);
