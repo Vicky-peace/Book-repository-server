960656735dd6113604ba4212bb1d9f4c7bb8739b
Vicky-peace <victorpeace47@gmail.com>
2024-06-27 09:32:13 +0300

    cors added

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import {Hono} from 'hono';
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import "dotenv/config";
 import {serve} from '@hono/node-server';
 
@@ -6,6 +7,9 @@ import {serve} from '@hono/node-server';
 import { booksRouter } from './book/book.router';
 
 const app = new Hono();
+
+// CORS for all routes
+app.use('*', cors())
 
 app.get('/', async (c) => {
     c.text('Welcome to Book API');
